ceefb065d29ca62d99ceb421e345fc0843f9eb14
Marcelo Arbori Nogueira <arbori@brq.com>
2018-05-10 20:19:31 -0300

    Eliminação das camadas dropout da rede feedforward.

--- ca_feedforward.py
+++ ca_feedforward.py
@@ -50,14 +50,16 @@ def make_set_pair_ne_label(total_ics, k, r, t, transient = 0):
     return data, label, rules
 
 ###############################################################################
-def model_function(features, labels, mode):
+def model_function_with_dropout(features, labels, mode):
     """Model function for CNN."""
     # Input Layer
     # Reshape X to 4-D tensor: [batch_size, width, height, channels]
     # Temporal Evolution are 100x100 cells, and have one color channel (black or white)
-    input_layer = tf.reshape(features["x"], [-1, 100, 100, 1])
+    #input_layer = tf.reshape(features["x"], [-1, 100, 100, 1])
 
-    flat = tf.reshape(input_layer, [-1, 100 * 100 * 1])
+    #flat = tf.reshape(input_layer, [-1, 100 * 100 * 1])
+
+    flat = features["x"]
 
     hiden_01 = tf.layers.dense(inputs=flat, units=10000, activation=tf.nn.sigmoid)
 
@@ -118,6 +120,58 @@ def model_function(features, labels, mode):
         mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
 
 ###############################################################################
+def model_function_without_dropout(features, labels, mode):
+    """Model function for CNN."""
+    # Input Layer
+    # Reshape X to 4-D tensor: [batch_size, width, height, channels]
+    # Temporal Evolution are 100x100 cells, and have one color channel (black or white)
+    input_layer = features["x"]
+
+    hiden_01 = tf.layers.dense(inputs=input_layer, units=10000, activation=tf.nn.sigmoid)
+    hiden_02 = tf.layers.dense(inputs=hiden_01, units=4000, activation=tf.nn.sigmoid)
+    hiden_03 = tf.layers.dense(inputs=hiden_02, units=1600, activation=tf.nn.sigmoid)
+    hiden_04 = tf.layers.dense(inputs=hiden_03, units=640, activation=tf.nn.sigmoid)
+    hiden_05 = tf.layers.dense(inputs=hiden_04, units=256, activation=tf.nn.sigmoid)
+    hiden_06 = tf.layers.dense(inputs=hiden_05, units=128, activation=tf.nn.sigmoid)
+    hiden_07 = tf.layers.dense(inputs=hiden_06, units=128, activation=tf.nn.sigmoid)
+    hiden_08 = tf.layers.dense(inputs=hiden_07, units=64, activation=tf.nn.sigmoid)
+    hiden_09 = tf.layers.dense(inputs=hiden_08, units=32, activation=tf.nn.sigmoid)
+    hiden_10 = tf.layers.dense(inputs=hiden_09, units=16, activation=tf.nn.sigmoid)
+    hiden_11 = tf.layers.dense(inputs=hiden_10, units=8, activation=tf.nn.sigmoid)
+
+    output_layer = tf.layers.dense(inputs=hiden_11, units=4)
+
+    predictions = {
+        # Generate predictions (for PREDICT and EVAL mode)
+        "classes": tf.argmax(input=output_layer, axis=1),
+        # Add `softmax_tensor` to the graph. It is used for PREDICT and by the
+        # `logging_hook`.
+        "probabilities": tf.nn.softmax(output_layer, name="softmax_tensor")
+    }
+
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
+
+    # Calculate Loss (for both TRAIN and EVAL modes)
+    loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=output_layer)
+
+    # Configure the Training Op (for TRAIN mode)
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+        train_op = optimizer.minimize(
+            loss=loss,
+            global_step=tf.train.get_global_step())
+        return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
+
+    # Add evaluation metrics (for EVAL mode)
+    eval_metric_ops = {
+        "accuracy": tf.metrics.accuracy(
+            labels=labels, predictions=predictions["classes"])}
+
+    return tf.estimator.EstimatorSpec(
+        mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+
+###############################################################################
 def train_function(k, r, t, transient, train_data_size, train_file, eval_file, model_diretory):
     eval_data_size = int(0.1*train_data_size)
     if eval_data_size <= 0:
@@ -168,7 +222,7 @@ def train_function(k, r, t, transient, train_data_size, train_file, eval_file, m
     # Create the Estimator
     print("Create the Estimator")
     ca_classifier = tf.estimator.Estimator(
-        model_fn=model_function, model_dir=model_diretory)
+        model_fn=model_function_without_dropout, model_dir=model_diretory)
 
     # Set up logging for predictions
     # Log the values in the "Softmax" tensor with label "probabilities"
@@ -209,10 +263,12 @@ def feedforward_classify_space_rule(k, r, size, total_ics, model_path, output_fi
     m = 2*r + 1
     rule_size = int(pow(k, pow(k, m)))
 
+    train_data_size = total_ics
+
     # Create the Estimator
     print("Create the Estimator")
     ca_classifier = tf.estimator.Estimator(
-        model_fn=model_function, model_dir=model_path)
+        model_fn=model_function_with_dropout, model_dir=model_path)
 
     # Predict CA in ray 1 space
     with open(output_file, mode='w', encoding=output_encoding) as a_file:
@@ -247,28 +303,46 @@ def feedforward_classify_space_rule(k, r, size, total_ics, model_path, output_fi
 
 ###############################################################################
 def main(argv):
-#    if len(argv) < 9:
-#        print("Usage: python ca_class.py k r t transient train_data_size train_file eval_file model_diretory output_file")
-#        return
+    if len(argv) < 1:
+        print("The proccess that wanted execute must be indicate, like:")
+        print("python ca_feedforward.py process ...")
+        print("Where process may be train or predict.")
 
-    print("Start training...")
+    process = argv[0]
 
-    k = int(argv[0])
-    r = float(argv[1])
-    t = int(argv[2])
-    transient = int(argv[3])
-    train_data_size = int(argv[4])
-    train_file = argv[5]
-    eval_file = argv[6]
-    model_diretory = argv[7]
+    if process == "train":
+        if len(argv) < 9:
+            print("Usage: python ca_class.py train k r t transient train_data_size train_file eval_file model_diretory")
+            return
 
-#    train_function(k, r, t, transient, train_data_size, train_file, eval_file, model_diretory)
+        print("Start training...")
 
-    size = t - transient
-    total_ics = train_data_size
-    model_path = model_diretory
-    output_file = eval_file
-    feedforward_classify_space_rule(k, r, size, total_ics, model_path, output_file)
+        k = int(argv[1])
+        r = float(argv[2])
+        t = int(argv[3])
+        transient = int(argv[4])
+        train_data_size = int(argv[5])
+        train_file = argv[6]
+        eval_file = argv[7]
+        model_diretory = argv[8]
+
+        train_function(k, r, t, transient, train_data_size, train_file, eval_file, model_diretory)
+    
+    elif process == "predict":
+        if len(argv) < 7:
+            print("Usage: python ca_class.py predict k r size total_ics model_path output_file")
+            return
+
+        print("Start predicting...")
+
+        k = int(argv[1])
+        r = float(argv[2])
+        size = int(argv[3])
+        total_ics = int(argv[4])
+        model_path = argv[5]
+        output_file = argv[6]
+
+        feedforward_classify_space_rule(k, r, size, total_ics, model_path, output_file)
 
 ###############################################################################
 if __name__ == "__main__":
